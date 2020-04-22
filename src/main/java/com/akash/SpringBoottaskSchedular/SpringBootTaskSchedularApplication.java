124b0493012df272ff8d22355daf0b50e887c1fe
Akashkumar4 <akash.kumar@oodlestechnologies.com>
2020-04-22 13:27:20 +0530

    taskSchudler first project

---
+++ src/main/java/com/akash/SpringBoottaskSchedular/SpringBootTaskSchedularApplication.java
@@ -0,0 +1,13 @@
+package com.akash.SpringBoottaskSchedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTaskSchedularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTaskSchedularApplication.class, args);
+	}
+
+}
